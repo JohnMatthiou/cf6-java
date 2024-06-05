1747b1e08c1427bfd8435c540c736f137f045aa1
Ioannis Matthioudakis <imatthioudakis@gmail.com>
2024-06-05 05:15:22 +0300

    Addition of ch14 and ch15

---
+++ src/gr/aueb/cf/ch15/immutable/ImmutablePoint3D2.java
@@ -0,0 +1,32 @@
+package gr.aueb.cf.ch15.immutable;
+
+public final class ImmutablePoint3D2 {
+    private final ImmutablePoint point;
+    private final int z;
+
+    public ImmutablePoint3D2() {
+        this.point = new ImmutablePoint();
+        this.z = 0;
+    }
+
+    public ImmutablePoint3D2(ImmutablePoint point, int z) {
+        this.point = point;
+        this.z = z;
+    }
+
+    public ImmutablePoint getPoint() {
+        return point;
+    }
+
+    public int getZ() {
+        return z;
+    }
+
+    @Override
+    public String toString() {
+        return "ImmutablePoint3D2{" +
+                "point=" + point +
+                ", z=" + z +
+                '}';
+    }
+}
