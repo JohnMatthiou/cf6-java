8c929f6da589f416af235d7c2f33753a67ab3992
Ioannis Matthioudakis <imatthioudakis@gmail.com>
2024-09-06 04:51:57 +0300

    Addition of ch25

---
+++ src/gr/aueb/cf/ch25/DateTimeServer.java
@@ -0,0 +1,34 @@
+package gr.aueb.cf.ch25;
+
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+/**
+ * Iterative Server.
+ */
+public class DateTimeServer extends Thread {
+
+    @Override
+    public void run() {
+        ServerSocket servFd;
+        int serverPort = 13;
+
+        try {
+            servFd = new ServerSocket();
+            servFd.bind(new InetSocketAddress("127.0.0.1", serverPort), 50);
+
+            while (true) {
+                Socket connectedFd = servFd.accept();
+                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(connectedFd.getOutputStream()));
+                bw.write(new Date().toString());
+                bw.flush();
+                connectedFd.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
